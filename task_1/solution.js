d9e4b454b5b52e7865c1c417260cf7840fe74f3a
izol <anna.novikova07@gmail.com>
2022-07-24 15:26:15 +0300

    Update solution.js

--- task_1/solution.js
+++ task_1/solution.js
@@ -10,7 +10,7 @@ function addInCartMessage(title, price) {
         message = message + 'за';
         message = message + ' ';
         message = message + productPrice + 'теперь в корзине!';
-    console.log (message);
+    console.log(message);
 
     // Конец решения задания №1.1.
 
