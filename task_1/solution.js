521950b50bcd59cea1258aa3aff0950120be540e
izol <anna.novikova07@gmail.com>
2022-07-24 14:58:33 +0300

    Update solution.js

--- task_1/solution.js
+++ task_1/solution.js
@@ -8,6 +8,7 @@ function addInCartMessage(title, price) {
         message = productName;
         message = message + ' ';
         message = message + 'за';
+        message = message + ' ';
         message = message + 'теперь в корзине!';
     console.log (message);
 
