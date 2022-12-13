2f87fad0f82574c65236535afae35f02f26d81e2
izol <anna.novikova07@gmail.com>
2022-12-13 15:41:10 +0300

    Update solution.js

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,11 +3,11 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
-    let product = {   // product - объект вида {id: id, title: title, count: count, price: price}
-        id: 1,
-        title: 'Пицца',
-        count: 5,
-        price: '500.00'
+    let goods = {   // product - объект вида {id: id, title: title, count: count, price: price}
+        id: 12,
+        title: 'Суши',
+        count: 6,
+        price: '450.00'
     };
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
@@ -21,14 +21,10 @@ function renderCartItem(item) {
     // 5 × 500.00 ₽ = 2500 ₽
     
     let itemCountText = "" // 1. Создайте переменную itemCountText, равную пустой строке
-    itemCountText = itemCountText + product.count // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
-    itemCountText = itemCountText + ` × ` // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    itemCountText = itemCountText + product.price // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    itemCountText = itemCountText +  ` ₽ = ` // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-    let sum = product.count * product.price // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-    itemCountText = itemCountText + Number(sum) // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
-    itemCountText = itemCountText + ` ₽` // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
+    
+    itemCountText = `${ goods.count} × ${goods.price} ₽ = ${goods.count * goods.price} ₽`;
 
+   
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
