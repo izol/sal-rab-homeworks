d5234daf0f09521d6ea1c3e36ec558cd54fbcc87
izol <anna.novikova07@gmail.com>
2022-12-14 07:45:03 +0300

    Update solution.js

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,25 +2,8 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
-
-    //let product = {   // product - объект вида {id: id, title: title, count: count, price: price}
-    //    id: 12,
-    //    title: 'Суши',
-    //    count: 6,
-    //    price: '450.00'
-    };
-    // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
-    // id - идентификатор
-    // title - наименование
-    // price - цена
-    // count - количество
-
-    // сформируйте строку itemCountText вида:
-    // количество × цена ₽ = стоимость ₽
-    // например,
-    // 5 × 500.00 ₽ = 2500 ₽
     
-    let itemCountText = "" // 1. Создайте переменную itemCountText, равную пустой строке
+    let itemCountText = "" 
     
     itemCountText = `${ product.count} × ${product.price} ₽ = ${product.count * product.price} ₽`;
 
