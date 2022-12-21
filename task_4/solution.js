f3f145f6ad348a67ba68824ffd5b4ef1a4f0cda9
izol <anna.novikova07@gmail.com>
2022-12-21 04:04:37 +0300

    Update solution.js

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,15 +3,24 @@
 function parseProducts(json) {
                                // Аргументом функции является JSON
 
-   let data = JSON.parse(json)
-   let products = $ {data.products}
-   return parseProducts[products]
+   let data = JSON.parse(json);
+   let products = ${data.products};
+   return products;
    
    // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
 }
+
+function renderProductsCards(json) {
+ clearProducts();
+ products = parseProducts(json);
+ let length = products.length; 
+   for (let i = 0; i < length; i += 1) {
+     addProduct(products[i]);
+}
+
 
 // Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
