23583284eeaab77af09120a5ee958a045962a12a
izol <anna.novikova07@gmail.com>
2023-02-10 11:30:04 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -46,7 +46,7 @@ function sendRequest (name, phone, address, goods, sum) {
     data.order.sum = sum;
 
 
-    let jsonData = JSON.stringify({data:name} + " " + {data:phone});
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
