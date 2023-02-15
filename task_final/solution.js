0631ca84bf1f790b97bd840d63049443d2b11df0
izol <anna.novikova07@gmail.com>
2023-02-15 20:49:15 +0300

    Update solution.js

--- task_final/solution.js
+++ task_final/solution.js
@@ -39,7 +39,7 @@ function sendRequest (name, phone, address, goods, sum) {
     let countOfGoods = goods.length;
 
     for (let i = 0; i < countOfGoods; i++) {
-        data.goods.push({title: goods[i], count: goods[i]});
+        data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
     data.order.address = `${data.address}`;
